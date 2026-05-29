04e124dc4cadcb8361ca11fa2384e5472c43ec07
Dogcell-Dev <pitermirandamacedopc1@gmail.com>
2026-05-29 12:57:27 -0300

    29/05/2026

---
+++ exer_logica/atv6.py
@@ -0,0 +1,9 @@
+def valorX (x):
+    return 3 * x + 1
+
+valores = [4,7,8,10,34,23,-16]
+
+for x in valores:
+        resultado = valorX(x)
+        print(f"O valor de x é: {x} e o resultado é {resultado}\n")
+
---
+++ exer_logica/atv7.py
@@ -0,0 +1,12 @@
+vertices = ["A","B","C","D"]
+
+arestas = [
+    ("A","B"),
+    ("A","C"),
+    ("B","D"),
+]
+
+print(f"Vértices {vertices}")
+print(f"Arestas {arestas}")
+print("Quantidade de Vértices", len(vertices))
+print("Quantidade de Arestas", len(arestas))
