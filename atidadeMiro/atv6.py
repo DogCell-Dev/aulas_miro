433142072d0345d0e7887f411fd8fe11b183ac48
Dogcell-Dev <pitermirandamacedopc1@gmail.com>
2026-06-04 00:07:57 -0300

    exercicio 6 concluido

--- atidadeMiro/atv6.py
+++ atidadeMiro/atv6.py
@@ -1,6 +1,31 @@
-vertices = ["Ana", "Bruno", "Carlos", "Daniela"]
+usuarios = ["Ana", "Bruno", "Carlos", "Daniela"]
 
-aresta = [("Ana","Bruno"), ("Ana","Carlos"), ("Bruno","Daniela"), ("Carlos","Daniela")]
+grafo = {usuario: [] for usuario in usuarios}
 
-print(f"Vértices -> {vertices}")
-print(f"Arestas -> {aresta}")
+relacionamentos = [
+    ("Ana", "Bruno"),
+    ("Ana", "Carlos"),
+    ("Bruno", "Daniela")
+]
+
+for u1, u2 in relacionamentos:
+    grafo[u1].append(u2)
+    grafo[u2].append(u1)
+
+graus = {}
+print("--- Grau de cada usuário ---")
+for usuario, conexoes in grafo.items():
+    grau = len(conexoes)
+    graus[usuario] = grau
+    print(f"Grau de {usuario}: {grau}")
+
+print("\n--- Resultado ---")
+maior_grau = max(graus.values())
+usuarios_mais_conectados = [user for user, grau in graus.items() if grau == maior_grau]
+
+if len(usuarios_mais_conectados) == 1:
+    print(f"O usuário com maior número de conexões é: {usuarios_mais_conectados[0]} ({maior_grau} conexões)")
+else:
+    lista_nomes = ", ".join(usuarios_mais_conectados)
+    print(f"Os usuários empatados com maior número de conexões são: {lista_nomes} ({maior_grau} conexões cada)")
+
